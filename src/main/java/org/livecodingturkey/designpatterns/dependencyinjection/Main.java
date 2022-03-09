736b6268bf234a5376641409a03adc93cde11181
Gülay ADIGÜZEL <44726684+GulayAdgzl@users.noreply.github.com>
2022-03-09 16:30:45 +0300

    Update Main.java

--- src/main/java/org/livecodingturkey/designpatterns/dependencyinjection/Main.java
+++ src/main/java/org/livecodingturkey/designpatterns/dependencyinjection/Main.java
@@ -1,5 +1,40 @@
 package org.livecodingturkey.designpatterns.dependencyinjection;
+//There are basically 3 types of ADI. These; 
+//Constructor Injection, Setter Injection, Method Injection) 
+//All methods rely on externally injecting their dependent classes
+
+//CustomerDal 
+public class CustomerDal implements ICustomerDal{
+  public void add(){
+    System.out.println("Oracle Veritabanı eklendi.");
+  }
+}
+//CustomerManager
+public class CustomerManager{
+  
+  private ICustomerManager(ICustomerDal customerDal){
+    this.customerDal=customerDal;
+  }
+  public void add(){
+    customerDal.add();
+  }
+}
+
+public interface ICustomerDal{
+  void add();
+}
+
+public class MySqlCustomerDal implements ICustomerDal{
+  @Override
+  public void add(){
+    System.out.println("Mysql veritabanına eklendi.");
+  }
+}
 
 public class Main {
+  CustomerManager manager=new CustomerManager(new  CustomerDal));
+  CustomerManager manager=new CustomerManager(new  MySqlCustomerDal));
+  
+  manager.add();
 
 }

b718609a3f90b4f0972c2aab819b512024faca5e
malizkl <63977569+malizkl@users.noreply.github.com>
2022-03-11 15:20:26 +0300

    made some changes according to the reviewer's feedbacks.

--- src/main/java/org/livecodingturkey/designpatterns/adapter/Main.java
+++ src/main/java/org/livecodingturkey/designpatterns/adapter/Main.java
@@ -1,11 +1,9 @@
 package org.livecodingturkey.designpatterns.adapter;
 
 
-// Java implementation of Adapter pattern
-
 interface Bird
 {
-    // birds implement Bird interface that allowss
+    // birds implement Bird interface that allows
     // them to fly and make sounds adaptee interface
     public void fly();
     public void makeSound();
